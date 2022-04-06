91013a50b27f67dc02785ef236a96c1a81afd5cc
MrSachin7 <82201769+MrSachin7@users.noreply.github.com>
2022-04-06 20:18:21 +0200

    WebApi added

---
+++ WebApi/Controllers/ForumController.cs
@@ -0,0 +1,15 @@
+﻿using Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers; 
+
+[ApiController]
+[Route("[controller]")]
+public class ForumController : ControllerBase {
+
+    private IForumService forumService;
+
+    public ForumController(IForumService forumService) {
+        this.forumService = forumService;
+    }
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,3 +1,7 @@
+using Application;
+using Contracts;
+using JsonDataAccess;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IUserService, UserServiceImpl>();
+builder.Services.AddScoped<IForumService, ForumServiceImpl>();
+builder.Services.AddScoped<IUserDAO, UserDAOImpl>();
+builder.Services.AddScoped<IForumDAO, ForumDAOImpl>();
+builder.Services.AddScoped<JsonForumContext>();
+builder.Services.AddScoped<JsonUserContext>();
 
 var app = builder.Build();
 
